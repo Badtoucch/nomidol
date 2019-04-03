ef718df3f6e0c5c5bcabbbd6f7c151580fc2cc1e
Vladislad T <v.trundenko@everad.com>
2019-04-03 16:26:31 +0300

    vlad

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,20 @@
 
 //=include ../components/J_footer/footer.js
+
+
+$(function(){
+	loopIt();
+});
+
+function loopIt(){    
+	$('.risky-content').each(function(i){
+		var t=$(this);
+		setTimeout(function(){
+			t.addClass('ractive3');
+				setTimeout(function(){
+					t.removeClass('ractive3');
+						if(i==$('.risky-content').length-1) loopIt();;
+				}, 3000);
+		}, 3100*i);
+	});
+}
